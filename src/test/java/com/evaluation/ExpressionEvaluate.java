a094cff0c574bd91dcf6a4f983013acaa0e430a6
saadaoui imane <saadaouiimane2004@gmail.com>
2024-12-30 12:34:19 +0100

    premier commit

--- src/test/java/com/evaluation/ExpressionEvaluate.java
+++ src/test/java/com/evaluation/ExpressionEvaluate.java
@@ -1,8 +1,17 @@
 package com.evaluation;
 
 public class ExpressionEvaluate {
+
+
     public static int evaluate(String chaine) {
             return 0;
     }
 
+
 }
+
+
+
+
+
+
--- src/test/java/com/evaluation/ExpressionEvaluationTest.java
+++ src/test/java/com/evaluation/ExpressionEvaluationTest.java
@@ -1,14 +1,53 @@
 package com.evaluation;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExpressionEvaluationTest {
 
     @Test
     void ChainVide() {
-        assertEquals("0",ExpressionEvaluate.evaluate (""));
+//Given
+        String chain="";
+        int nbr =0;
+//when
+        int resultat=ExpressionEvaluate. evaluate(chain);
+//Then
+        assertThat(resultat).isEqualTo(nbr);
+
+    }
+
+    @Test
+    void SeulNombre() {
+
+        //given
+        String chain="5";
+        int nbr=5;
+
+        //when
+        int resultat=ExpressionEvaluate.evaluate(chain);
+
+        //then
+        assertThat(resultat).isEqualTo(nbr);
+       // assertEquals("5",ExpressionEvaluate.evaluate ("5"));
+    }
+
+    @Test
+    void testdeuxNumbers() {
+        String chain="1,2";
+        int nbr =3;
+        int resultat=ExpressionEvaluate.evaluate(chain);
+        assertThat(resultat).isEqualTo(nbr);
+
+    }
+
+    @Test
+    void testMultipleNumbers() {
+        String chain="1,2,3,4,5";
+        int nbr =15;
+        int resultat=ExpressionEvaluate.evaluate(chain);
+        assertThat(resultat).isEqualTo(nbr);
 
     }
 
